a88382ec956f53683d21863be1677780aac8d496
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-10-01 19:37:37 +0530

    Add files via upload

---
+++ random superhero/index.js
@@ -0,0 +1,9 @@
+// import generateName from 'sillyname';
+import  superheroes from 'superheroes';
+
+
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`);
+
+const name = superheroes.random();
+console.log(`My name is ${name}.`);
